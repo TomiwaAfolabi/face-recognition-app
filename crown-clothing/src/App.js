44f1e0d57285196801e80e81f52758c11c0b977a
Tomiwa  Afolabi <tomiwaafolabi@Tomiwas-MacBook-Air.local>
2023-01-30 15:42:49 +0100

    crown app

---
+++ crown-clothing/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Category from "./components/category-list/category-container.component";
+
+function App() {
+  return (
+    <div>
+      <Category />
+    </div>
+  );
+}
+
+export default App;
---
+++ crown-clothing/src/components/category-item/category-body.component.jsx
@@ -0,0 +1,19 @@
+function CategoryItem({ category }) {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h1>{title}</h1>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
